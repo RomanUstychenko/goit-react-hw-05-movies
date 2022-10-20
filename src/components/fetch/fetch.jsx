403ad3222dea824180ad628b92aed6373981ccd8
RomanUstychenko <roman.ustychenko@gmail.com>
2022-10-20 22:08:52 +0300

    Update fetch.jsx

--- src/components/fetch/fetch.jsx
+++ src/components/fetch/fetch.jsx
@@ -1,44 +1,44 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3/movie/550",
-    params: {
-        per_page: 12,
-        key: "04cb754e3fc264242ddc48b1c691d765"
-    }
-})
-export const getImage = async ( page=1) => {
-    const {data} = await instance.get("/", {
-        params: {
-            page,
-        }
-    });
-    return data
-}
-export const searchImage = async (q, page=1) => {
-    const {data} = await instance.get("/", {
-        params: {
-            q,
-            page,
-        }
-    });
-    return data
-}
+// const instance = axios.create({
+//     baseURL: "https://api.themoviedb.org/3/movie/550",
+//     params: {
+//         per_page: 12,
+//         key: "04cb754e3fc264242ddc48b1c691d765"
+//     }
+// })
+// export const getImage = async ( page=1) => {
+//     const {data} = await instance.get("/", {
+//         params: {
+//             page,
+//         }
+//     });
+//     return data
+// }
+// export const searchImage = async (q, page=1) => {
+//     const {data} = await instance.get("/", {
+//         params: {
+//             q,
+//             page,
+//         }
+//     });
+//     return data
+// }
 
 
-https://api.themoviedb.org/3/movie/550?api_key=04cb754e3fc264242ddc48b1c691d765
+// https://api.themoviedb.org/3/movie/550?api_key=04cb754e3fc264242ddc48b1c691d765
 
-const KEY = '04cb754e3fc264242ddc48b1c691d765';
-const BASE_URL = 'https://api.themoviedb.org/3/movie/550';
-const LIMIT = 12;
+// const KEY = '04cb754e3fc264242ddc48b1c691d765';
+// const BASE_URL = 'https://api.themoviedb.org/3/movie/550';
+// const LIMIT = 12;
 
-export const fetch = async (query, page) => {
-    try {
-        const response = await axios.get
-        (`${BASE_URL}/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${LIMIT}`);
-return response.data
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
+// export const fetch = async (query, page) => {
+//     try {
+//         const response = await axios.get
+//         (`${BASE_URL}/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${LIMIT}`);
+// return response.data
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+// }
